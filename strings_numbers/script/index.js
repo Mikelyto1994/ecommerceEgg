7e93f223991562e8644308ed6671a7716e75397f
iribel97 <irinaochoa32@gmail.com>
2024-04-11 20:17:56 -0500

    Conocer la longitud de una frase

--- strings_numbers/script/index.js
+++ strings_numbers/script/index.js
@@ -5,7 +5,12 @@ console.log(typeof numero);
 const cadena = "Cadena de texto";
 console.log(cadena);
 console.log(typeof cadena);
+console.log("La longitud de la variable cadena es: " + cadena.length);
 
 const booleano = true;
 console.log(booleano);
 console.log(typeof booleano);
+
+/* saber la longitud de la cadena de texto */
+const nuevaCadena = "Hallar la longitud de esta cadena";
+console.log(nuevaCadena.length);
