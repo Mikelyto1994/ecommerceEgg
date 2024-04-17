3a076f462c41ea20ea79a4ec795cc498f9746a7d
iribel97 <irinaochoa32@gmail.com>
2024-04-17 18:35:34 -0500

    Agregar un atributo privado y creando sus respectivos getter y setter

--- store/scripts/product.js
+++ store/scripts/product.js
@@ -1,12 +1,23 @@
 class Product {
     //constructor
-    constructor(id, name, price, stock, photo, onsale) {
+    constructor(id, name, price, stock, photo, onsale, supplier) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.stock = stock;
         this.photo = photo;
         this.onsale = onsale;
+        //Atributo privado
+        this._supplier = supplier;
+    }
+
+    //metodos getter y setter del atributo privado
+    get supplier() {
+        return this._supplier;
+    }
+
+    set supplier(newSupplier) {
+        this._supplier = newSupplier;
     }
 }
 
@@ -16,6 +27,14 @@ const product1 = new Product();
 const product2 = new Product("0002", "Tablet", 1200);
 //variable prod3 para crear una instancia de Product con todas las propiedades que requiere la clase.
 const product3 = new Product("0003", "Smartphone", 800, 5, "https://granhogar.com.ec/imagenes/productos/7988:49:d0?v=0", true);
+//prod4 para crear una instancia de Product con todas las propiedades que requiere la clase. 
+const product4 = new Product("0004", "Smartwatch", 300, 10, "https://granhogar.com.ec/imagenes/productos/7988:49:d0?v=0", false, "Samsung");
+
+
+//modificar valor de atributo privado
+product4.supplier = "Samsung Electronics";
+//Programa la impresión en consola del getter de prod4. 
+console.log(`El proveedor del producto 4 es: ${product4.supplier}`);
 
 //impresion por consola
 console.log("Productos: ");
