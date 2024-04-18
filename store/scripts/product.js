cad8ef18621796777fd835da0d3b6778f4607202
iribel97 <irinaochoa32@gmail.com>
2024-04-18 00:16:29 -0500

    POO2 Ejercicio: Enriquecimiento de información de productos. Finalizado

--- store/scripts/product.js
+++ store/scripts/product.js
@@ -1,7 +1,7 @@
 // clase Producto
 class Product {
     //constructor
-    constructor(id, name, price, stock, photo, onsale, supplier) {
+    constructor(id, name, price, stock, photo, onsale, supplier, colors, description) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -10,7 +10,12 @@ class Product {
         this.onsale = onsale;
         //Atributo privado
         this._supplier = supplier;
+        //Array de colores
+        this.colors = colors;
+        //Descripción
+        this.description = description;
     }
+
 
     //metodos getter y setter del atributo privado
     get supplier() {
@@ -29,41 +34,36 @@ class Product {
         }
 		this.stock = this.stock - units
 	}
+
+    //Imprimir todos los datos del producto
+    printProduct() {
+        console.log("------------------------------------------------------------------------------------------------");
+        console.log("ID: " + this.id);
+        console.log("Nombre: " + this.name);
+        console.log("Precio: " + this.price);
+        console.log("Stock: " + this.stock);
+        console.log("Fotos: ");
+        for (let i = 0; i < this.photo.length; i++) {
+            console.log("   - " + this.photo[i]);
+        }
+        console.log("En oferta: " + this.onsale);
+        console.log("Proveedor: " + this._supplier);
+        console.log("Colores: ");
+        for (let i = 0; i < this.colors.length; i++) {
+            console.log("   - " + this.colors[i]);
+        }
+        console.log("Descripción: " + this.description);
+    }
 }
 
 //instanciamos Los objetos
-const product1 = new Product("001", "Laptop", 1500, 20, "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c08484411.png", true, "HP");
-const product2 = new Product("002", "Mouse", 20, 50, "https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/pdp/accessories/wireless-mouse-gt/specs-img.jpg", false, "Genius");
-const product3 = new Product("003", "Keyboard", 50, 30, "https://media.wired.com/photos/65b0438c22aa647640de5c75/master/pass/Mechanical-Keyboard-Guide-Gear-GettyImages-1313504623.jpg", true, "Logitech");
-const product4 = new Product("004", "Monitor", 300, 10, "https://media.es.wired.com/photos/63b59dd32984c2acd03054a8/16:9/w_1280,c_limit/Samsung-%20Odyssey-Neo-G9-Gaming-Monitor-G95NC.png", false, "Samsung");
+const product1 = new Product("001", "Laptop", 1500, 20, ["https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c08484411.png", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrbMzhmpLxmrGdPKoOLuvmQ26C-BTP2eQRS8dk-8arCOp2G6NazGU15JenGjFF1qZZI3w&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwNaMBfQ_eQiuLkHUUbc45L9FEOzDLaeOerJvxm3Ykl1SA9chGGsKJsaLJGcmNUBCfgRY&usqp=CAU"], true, "HP", ["black", "white", "blue"], "Laptop HP Omen 15");
+const product2 = new Product("002", "Mouse", 20, 50, ["https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/pdp/accessories/wireless-mouse-gt/specs-img.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3Gzb74K6JnfjjUzdiEZnCEDEGBdJH8xEh5C7x0TmjsuTSgpVMpj6eqLMbudShUH7j050&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ12K0NE8mjIvx8rwWuItN-kudTD0-xyQTANF_a3agWt5CkWIxqqX5sKPKL50oytsO-rRw&usqp=CAU"], false, "Genius", ["black", "white"], "Mouse Genius");
+const product3 = new Product("003", "Keyboard", 50, 30, ["https://media.wired.com/photos/65b0438c22aa647640de5c75/master/pass/Mechanical-Keyboard-Guide-Gear-GettyImages-1313504623.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlY3xMwZJdtA9vAJmZXhvjU46nhZ8QfXqpQ4psVATpfy7q3pz01iumwQAD_xJOjBx5tfo&usqp=CAU"], true, "Logitech", ["black", "white", "red"], "Keyboard Logitech G Pro");
+const product4 = new Product("004", "Monitor", 300, 10, ["https://media.es.wired.com/photos/63b59dd32984c2acd03054a8/16:9/w_1280,c_limit/Samsung-%20Odyssey-Neo-G9-Gaming-Monitor-G95NC.png", "https://m.media-amazon.com/images/I/41+GCV1D9bL._SL500_.jpg"], false, "Samsung", ["black", "white", "gray", "blue"], "Monitor Samsung Odyssey Neo G9");
 
 
 //Creamos el array de los productos
 let products = [product1, product2, product3, product4];
 
-console.log(products);
-
-//Definiendo variables product5 y product6
-const product5 = new Product("005", "Headphones", 100, 25, "https://www.sony.com/image/7d5f0d8f6e3b6f0b1c5e4a5c7e7e3f4d?fmt=pjpeg&wid=330&hei=330&bgcolor=FFFFFF&bgc=FFFFFF", true, "Sony");
-const product6 = new Product("006", "Webcam", 70, 15, "https://www.logitech.com/content/dam/logitech/en/products/webcams/streamcam/streamcam-gallery-1.png", false, "Logitech");
-
-//Agregar product5 al inicio del array
-products.unshift(product5);
-
-//Agregar product6 al final del array
-products.push(product6);
-
-//Imprimir por consola el array
-console.log("Array de productos luego de agregar: ");
-console.log(products);
-
-//Quitar el primer elemento del array
-products.shift();
-
-//Quitar el último elemento del array
-products.pop();
-
-//Imprimir por consola el array
-console.log("Array de productos luego de quitar: ");
-console.log(products);
-
+products.forEach(product => product.printProduct());
