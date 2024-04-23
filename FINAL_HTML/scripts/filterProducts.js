955e4b65264be6c7766e389817a1e5ce2163e5f4
iribel97 <irinaochoa32@gmail.com>
2024-04-23 12:44:38 -0500

    Actividad: Filtrar productos. Finalizada

--- FINAL_HTML/scripts/filterProducts.js
+++ FINAL_HTML/scripts/filterProducts.js
@@ -1,7 +1,13 @@
 //captureText para que imprima por consola “capturado”. 
 const captureText = (event) => {
-    console.log(event);
-    console.log(`Capturado: ${event.target.value}`);
+    //guardar en una variable el valor capturado por el evento
+    let text = event.target.value;
+
+    //filtre el array con todos los productos con los productos cuyo nombre incluya el texto capturado
+    let filteredProducts = products.filter(product => product.title.toLowerCase().includes(text.toLowerCase()));
+
+    //actualice la vista con una nueva impresión de las tarjetas
+    printCards(filteredProducts, "products");
 }
 
 // definir una variable searchSelector
